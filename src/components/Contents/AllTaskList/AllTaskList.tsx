b915b108b82107bc5e99d989ee9f792193d2d240
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-01-01 13:57:04 +0100

    Create the TaskCard

--- src/components/Contents/AllTaskList/AllTaskList.tsx
+++ src/components/Contents/AllTaskList/AllTaskList.tsx
@@ -1,7 +1,14 @@
+import TaskCard from "../../TaskCard/TaskCard";
 import "./AllTaskList.scss";
 
 function AllTaskList() {
-  return <div>AllTaskList</div>;
+  return (
+    <section className="all-task-list">
+      <div className="all-task-list__container">
+        <TaskCard />
+      </div>
+    </section>
+  );
 }
 
 export default AllTaskList;
--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,7 +1,34 @@
+import { MdDelete, MdEditDocument } from "react-icons/md";
 import "./TaskCard.scss";
 
 function TaskCard() {
-  return <div>TaskCard</div>;
+  return (
+    <section className="task-card">
+      <div className="task-card__container">
+        <h2 className="task-card__title">Task Title</h2>
+        <div className="task-card__description">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit,
+            consectetur adipisicing elit. Velit, consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="task-card__date">
+          <p>01/01/2025</p>
+        </div>
+        <div className="task-card__buttom">
+          <button>Completed</button>
+          <div className="task-card__buttom__icons">
+            <div className="task-card__buttom__icons__edit">
+              <MdEditDocument />
+            </div>
+            <div className="task-card__buttom__icons__delete">
+              <MdDelete />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default TaskCard;
